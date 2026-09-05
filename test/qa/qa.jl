38aac7464aa96fcbcb6700e2690d4da283412139
singhharsh1708 <hs1663531@gmail.com>
2026-09-05 22:04:37 +0530

    Mark the public-access check broken for the PETSc bindings

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,3 +1,4 @@
 using SciMLTesting, PETScDiffEq, Test
 
-run_qa(PETScDiffEq)
+# PETSc.jl declares no names public, so every call into it is a non-public access.
+run_qa(PETScDiffEq; ei_broken = (:all_qualified_accesses_are_public,))
