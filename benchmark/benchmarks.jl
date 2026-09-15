549153107075103fa7d573efb5bbd3668a351621
singhharsh1708 <hs1663531@gmail.com>
2026-09-15 18:13:06 +0530

    Benchmark TSARKIMEX, TSIRK, TSMPRK and TSDAE

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -69,3 +69,61 @@ SUITE["tsimplicit"]["oscillator_cn"] = @benchmarkable solve(
 SUITE["tsimplicit"]["lv_bdf"] = @benchmarkable solve(
     $prob_lv, PETScDiffEq.TSImplicit("bdf"); dt = 0.05
 )
+
+function stiff_relaxation!(du, u, p, t)
+    du[1] = -1000.0 * (u[1] - cos(t))
+    return nothing
+end
+
+function relaxation_forcing!(du, u, p, t)
+    du[1] = -sin(t)
+    return nothing
+end
+
+function two_rates!(du, u, p, t)
+    du[1] = -u[1]
+    du[2] = -100.0 * u[2]
+    return nothing
+end
+
+function algebraic_decay!(r, du, u, p, t)
+    r[1] = du[1] + u[1]
+    return r[2] = u[2] - u[1]
+end
+
+function algebraic_decay_jac!(J, du, u, p, gamma, t)
+    J[1, 1] = gamma + 1.0
+    J[1, 2] = 0.0
+    J[2, 1] = -1.0
+    return J[2, 2] = 1.0
+end
+
+prob_split = SplitODEProblem(stiff_relaxation!, relaxation_forcing!, [1.0], (0.0, 1.0))
+prob_two_rates = ODEProblem(two_rates!, [1.0, 1.0], (0.0, 1.0))
+prob_dae = DAEProblem(
+    DAEFunction(algebraic_decay!; jac = algebraic_decay_jac!),
+    [-1.0, -1.0], [1.0, 1.0], (0.0, 1.0)
+)
+
+SUITE["tsarkimex"] = BenchmarkGroup()
+SUITE["tsarkimex"]["split_3"] = @benchmarkable solve(
+    $prob_split, PETScDiffEq.TSARKIMEX("3"); dt = 0.01
+)
+SUITE["tsarkimex"]["oscillator_3"] = @benchmarkable solve(
+    $prob_osc, PETScDiffEq.TSARKIMEX("3"); dt = 0.05
+)
+
+SUITE["tsirk"] = BenchmarkGroup()
+SUITE["tsirk"]["oscillator_3stage"] = @benchmarkable solve(
+    $prob_osc, PETScDiffEq.TSIRK(3); dt = 0.05
+)
+
+SUITE["tsmprk"] = BenchmarkGroup()
+SUITE["tsmprk"]["two_rates_p2"] = @benchmarkable solve(
+    $prob_two_rates, PETScDiffEq.TSMPRK([1], "p2"); dt = 0.01, adaptive = false
+)
+
+SUITE["tsdae"] = BenchmarkGroup()
+SUITE["tsdae"]["algebraic_decay_bdf"] = @benchmarkable solve(
+    $prob_dae, PETScDiffEq.TSDAE("bdf"); dt = 1.0e-3
+)
